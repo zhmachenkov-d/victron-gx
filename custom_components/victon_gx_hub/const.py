3d53aa7c71fa28db553f8fbdd3b156dd0dda5964
Denys Zhmachenkov <dez@dentavision.dk>
2026-05-31 11:30:27 +0000

    feat: add integration config and binary sensor constants
    Define installation/serial config keys and binary sensor on/off enum ids used by the hub and platforms.

--- custom_components/victon_gx_hub/const.py
+++ custom_components/victon_gx_hub/const.py
@@ -1,3 +1,10 @@
 """Constants for the Victron GX integration."""
 
 DOMAIN = "victon_gx_hub"
+
+CONF_INSTALLATION_ID = "installation_id"
+CONF_SERIAL = "serial"
+
+# Binary sensor enum ids must be "on" for on and "off" for off.
+BINARY_SENSOR_ON_ID = "on"
+BINARY_SENSOR_OFF_ID = "off"
